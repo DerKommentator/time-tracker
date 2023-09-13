40c61edd4ed00847ec604ac95d7163b258031191
DerKommentator <22379432+DerKommentator@users.noreply.github.com>
2023-09-13 15:37:53 +0200

    - reponsive design changes; - add chart.js; - new statistics page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,58 @@
 <script lang="ts">
-	import { IconSettings } from '@tabler/icons-svelte';
+	import { IconChartPie, IconHome, IconMenu2, IconSettings } from '@tabler/icons-svelte';
 
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
-	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, LightSwitch, Drawer } from '@skeletonlabs/skeleton';
+	import { getDrawerStore, initializeStores } from '@skeletonlabs/skeleton';
+	initializeStores();
+	const drawerStore = getDrawerStore();
 </script>
 
 <svelte:head>
 	<title>TimeTracker</title>
 </svelte:head>
 
+<Drawer>
+	<div class="flex flex-col items-stretch gap-y-3 p-4">
+		<a class="btn variant-ringed" href="/" on:click={() => drawerStore.close()}>
+			<IconHome /><span class="text-lg font-semibold">Home</span>
+		</a>
+		<a class="btn variant-ringed" href="/statistics" on:click={() => drawerStore.close()}>
+			<IconChartPie /><span class="text-lg font-semibold">Statistiken</span>
+		</a>
+		<a class="btn variant-ringed" href="/settings" on:click={() => drawerStore.close()}>
+			<IconSettings /><span class="text-lg font-semibold">Einstellungen</span>
+		</a>
+	</div>
+</Drawer>
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar>
+		<AppBar
+			gridColumns="grid-cols-3"
+			slotDefault="place-self-center text-center lg:text-left"
+			slotTrail="place-content-end"
+		>
 			<svelte:fragment slot="lead">
-				<a class="text-xl uppercase" href="/">TimeTracker</a>
+				<a class="text-xl uppercase" href="/">
+					<span class="hidden lg:inline-flex">TimeTracker</span>
+					<!--TODO: LOGO-->
+					<span class="inline-flex lg:hidden">TT</span>
+				</a>
 			</svelte:fragment>
+			<button
+				class="btn variant-ringed inline-flex lg:hidden"
+				on:click={() => drawerStore.open({ position: 'top', height: 'h-fit' })}
+				><IconMenu2 />
+			</button>
+			<a class="btn variant-ringed hidden lg:inline-flex" href="/">
+				<IconHome /><span class="text-lg font-semibold">Home</span>
+			</a>
+			<a class="btn variant-ringed hidden lg:inline-flex" href="/statistics">
+				<IconChartPie /><span class="text-lg font-semibold">Statistiken</span>
+			</a>
 			<svelte:fragment slot="trail">
-				<a class="btn" href="/settings">
+				<a class="btn hidden md:inline-flex" href="/settings">
 					<IconSettings />
 				</a>
 				<LightSwitch />
---
+++ src/routes/statistics/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { IconArrowBack } from '@tabler/icons-svelte';
+	import { onMount } from 'svelte';
+	import { Chart } from 'chart.js/auto';
+	let canvas: HTMLCanvasElement;
+
+	let chart: Chart;
+
+	function createPieChart(ctx: CanvasRenderingContext2D) {
+		chart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: ['group 1', 'group 2'],
+				datasets: [
+					{
+						backgroundColor: '#ffffff',
+						borderWidth: 3,
+						label: 'test1',
+						data: [1, 3]
+					},
+					{
+						backgroundColor: '#00ff00',
+						borderWidth: 3,
+						label: 'test2',
+						data: [1, 3]
+					}
+				]
+			},
+			options: {}
+		});
+	}
+
+	onMount(() => {
+		const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+		createPieChart(ctx);
+	});
+</script>
+
+<div>
+	<a class="btn variant-filled-primary m-10" href="/">
+		<IconArrowBack />
+	</a>
+
+	<div class="card p-4 mx-5"><canvas bind:this={canvas}>{chart}</canvas></div>
+</div>
